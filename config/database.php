3ddeace9449e81fc09fa49b56b15d0eddf5b1932
tidianemohamed <mohamedtidiane9@gmail.com>
2026-06-16 12:38:30 +0200

    Array di configurazione database reso dinamico per Railway

--- config/database.php
+++ config/database.php
@@ -1,6 +1,22 @@
 <?php
 // config/database.php
 
+$databaseUrl = getenv('DATABASE_URL');
+
+if ($databaseUrl) {
+    // Se siamo su Railway, estraiamo i dati dalla stringa magica
+    $dbconnections = parse_url($databaseUrl);
+    
+    return [
+        'host'     => $dbconnections['host'],
+        'port'     => $dbconnections['port'] ?? '5432',
+        'database' => ltrim($dbconnections['path'], '/'),
+        'username' => $dbconnections['user'],
+        'password' => $dbconnections['pass'],
+    ];
+}
+
+// Se siamo in locale sul Mac, usiamo i tuoi dati originali
 return [
     'host'     => 'localhost',
     'port'     => '5432',
